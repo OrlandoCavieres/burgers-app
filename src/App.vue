5563087ea9451fac90477d83e8308e8e96b4fd7a
Orlando Cavieres <frizortake@gmail.com>
2021-11-06 15:51:47 -0300

    Creacion de estilos globales usando sass, ajuste de colores, tamanos y responsive generales.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/hamburguesas">Hamburguesas</router-link>
+    <div id="homeRoute">
+      <router-link to="/">Home</router-link>
+    </div>
+    <div id="burgerRoute">
+      <router-link to="/hamburguesas">Hamburguesas</router-link>
+    </div>
   </div>
-  <router-view/>
+  <div class="separador" />
+  <router-view id="route-v"/>
 </template>
 
-<style>
+<style lang="scss">
+@import 'globalStyles';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $dark-select-color;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
 }
 
 * {
@@ -21,15 +31,47 @@
 }
 
 #nav {
+  align-self: center;
+  display: flex;
   padding: 30px;
+  width: 100%;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: $dark-select-color;
+  background: transparent;
 }
 
 #nav a.router-link-exact-active {
-  color: #247BA0;
+  color: $light-select-color;
+
+}
+
+#homeRoute {
+  flex: 1;
+  background: transparent;
+}
+
+#homeRoute:before {
+  background-color: $alternative-color;
+  color: white;
+}
+
+#burgerRoute {
+  flex: 1;
+  background: transparent;
+}
+
+.separador {
+  background: $primary-color;
+  border-radius: $rad-smooth;
+  elevation: lower;
+  height: 7px;
+  width: 90%;
+}
+
+#route-v {
+  width: 100%;
 }
 </style>
