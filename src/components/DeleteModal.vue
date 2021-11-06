b3b7e067ce24e6e8ebb31c405565b2f6444483fb
Orlando Cavieres <frizortake@gmail.com>
2021-11-06 16:45:09 -0300

    Creacion inicial de modal para eliminacion

---
+++ src/components/DeleteModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <transition>
+
+    <div>
+      <div>
+        <div>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'DeleteModal'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
